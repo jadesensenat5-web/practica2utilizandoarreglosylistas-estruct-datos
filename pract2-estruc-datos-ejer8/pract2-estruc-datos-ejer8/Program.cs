647f820a1eb6fc3baae1e3dd1b46e166b672cff2
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-10 11:50:10 -0400

    Create Program.cs
    ejercicio8

---
+++ pract2-estruc-datos-ejer8/pract2-estruc-datos-ejer8/Program.cs
@@ -0,0 +1,9 @@
+﻿// 8. Positivos, Negativos o Ceros
+
+int p = 0, n = 0, z = 0;
+for (int i = 1; i <= 10; i++)
+{
+    double x = double.Parse(Console.ReadLine());
+    if (x > 0) p++; else if (x < 0) n++; else z++;
+}
+Console.WriteLine($"Pos: {p}, Neg: {n}, Ceros: {z}");
