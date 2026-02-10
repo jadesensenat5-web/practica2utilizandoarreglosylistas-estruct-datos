afc3e2f363e157889bff5e9d6b42971e330d1b0a
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-10 11:08:17 -0400

    Create Program.cs
    ejercicio5

---
+++ pract2-estruc-datos-ejer5/pract2-estruc-datos-ejer5/Program.cs
@@ -0,0 +1,9 @@
+﻿// 5. El mayor de 10 números
+double mayor = -999999;
+for (int i = 1; i <= 10; i++)
+{
+    Console.Write("Número: ");
+    double n = double.Parse(Console.ReadLine());
+    if (n > mayor) mayor = n;
+}
+Console.WriteLine("El mayor es: " + mayor);
