4cfccc01b223d756aeb6c3fbec163a39c4eed19d
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-10 10:54:30 -0400

    Create Program.cs
    ejercicio4

---
+++ pract2-estruc-datos-ejer4/pract2-estruc-datos-ejer4/Program.cs
@@ -0,0 +1,6 @@
+﻿// 4. Mínimo Común Múltiplo
+Console.Write("A: "); int a = int.Parse(Console.ReadLine());
+Console.Write("B: "); int b = int.Parse(Console.ReadLine());
+int m = (a > b) ? a : b;
+while (m % a != 0 || m % b != 0) m++;
+Console.WriteLine("MCM: " + m);
