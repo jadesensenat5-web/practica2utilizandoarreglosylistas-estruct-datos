2cec01d5fea81c234b45bdbf11a9be0e85a3ad03
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-10 10:34:59 -0400

    Create Program.cs
    ejercicio3

---
+++ pract2-estruc-datos-ejer3/pract2-estruc-datos-ejer3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Media de 5 números
+
+double s = 0;
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write("Num " + i + ": ");
+    s += double.Parse(Console.ReadLine());
+}
+Console.WriteLine("Media: " + (s / 5));
