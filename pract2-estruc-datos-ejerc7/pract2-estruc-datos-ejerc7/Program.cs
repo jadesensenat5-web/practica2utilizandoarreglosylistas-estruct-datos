86ee550131bf36d8a01be7c131d39d5e6e1cca48
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-10 11:44:05 -0400

    Create Program.cs
    ejercicio7

---
+++ pract2-estruc-datos-ejerc7/pract2-estruc-datos-ejerc7/Program.cs
@@ -0,0 +1,6 @@
+﻿// 7. Múltiplos de 5 entre dos números
+
+Console.Write("A: "); int a = int.Parse(Console.ReadLine());
+Console.Write("B: "); int b = int.Parse(Console.ReadLine());
+for (int i = a; i <= b; i++)
+    if (i % 5 == 0) Console.WriteLine(i);
