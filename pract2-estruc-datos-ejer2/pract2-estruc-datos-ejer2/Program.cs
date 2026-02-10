de22dbd314cc341f598930635c83428fe5429c2d
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-10 10:25:15 -0400

    Create Program.cs
    ejercicio2

---
+++ pract2-estruc-datos-ejer2/pract2-estruc-datos-ejer2/Program.cs
@@ -0,0 +1,10 @@
+﻿//2. Número Perfecto
+
+Console.Write("Pon el número: ");
+int n = int.Parse(Console.ReadLine()), s = 0, i = 1;
+while (i < n)
+{
+    if (n % i == 0) s += i;
+    i++;
+}
+Console.WriteLine(s == n ? "Es perfecto" : "No es perfecto");
