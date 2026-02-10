60a962c78764c307272c4bb95d2c79516ca3e696
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-10 11:21:45 -0400

    Create Program.cs
    ejercicio6

---
+++ pract2-estruc-datos-ejer6/pract2-estruc-datos-ejer6/Program.cs
@@ -0,0 +1,11 @@
+﻿// 6. Contar Primos
+
+int[] nums = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+int c = 0;
+foreach (int n in nums)
+{
+    int div = 0;
+    for (int i = 1; i <= n; i++) if (n % i == 0) div++;
+    if (div == 2) c++;
+}
+Console.WriteLine("Primos: " + c);
