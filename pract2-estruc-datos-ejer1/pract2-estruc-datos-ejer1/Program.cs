14196ae4de626214c0b15e18f2afcc95f9a902e6
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-10 10:14:43 -0400

    Create Program.cs
    ejercicio1

---
+++ pract2-estruc-datos-ejer1/pract2-estruc-datos-ejer1/Program.cs
@@ -0,0 +1,7 @@
+﻿// 1. programa que solicite al usuario un número y cuente cuántos números pares hay entre 1 y ese número utilizando FOR.
+
+Console.Write("Número: ");
+int n = int.Parse(Console.ReadLine()), c = 0;
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0) c++;
+Console.WriteLine("Pares: " + c);
