a2af1db0363f835a3d0743c65624209436cb6ee0
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-11 09:54:24 -0400

    Create Program.cs
    ejercicio19

---
+++ pract2-estruc-datos-ejer19/pract2-estruc-datos-ejer19/Program.cs
@@ -0,0 +1,8 @@
+﻿//19. Comprobar si un valor está presente
+
+int[] lista = { 10, 20, 30, 40 };
+Console.Write("Buscar número: ");
+int buscar = int.Parse(Console.ReadLine());
+bool existe = false;
+foreach (int n in lista) if (n == buscar) existe = true;
+Console.WriteLine(existe ? "Si está" : "No está");
