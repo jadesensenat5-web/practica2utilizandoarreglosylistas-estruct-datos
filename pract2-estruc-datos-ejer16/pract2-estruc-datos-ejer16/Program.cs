8dc7a2ee05db9429ae1789ec1b6e228323811e7b
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-11 09:17:31 -0400

    Create Program.cs
    ejercicio16

---
+++ pract2-estruc-datos-ejer16/pract2-estruc-datos-ejer16/Program.cs
@@ -0,0 +1,9 @@
+﻿// 16. Nueva lista multiplicada por 2
+
+int[] original = { 1, 2, 3, 4 };
+int[] nueva = new int[original.Length];
+for (int i = 0; i < original.Length; i++)
+{
+    nueva[i] = original[i] * 2;
+    Console.WriteLine(nueva[i]);
+}
