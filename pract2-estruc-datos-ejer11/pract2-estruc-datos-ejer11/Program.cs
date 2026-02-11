f2d1afc145fe6ad6e7b0c4718deea1f0bfab7628
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-11 07:59:14 -0400

    Create Program.cs
    ejercicio11-12

---
+++ pract2-estruc-datos-ejer11/pract2-estruc-datos-ejer11/Program.cs
@@ -0,0 +1,7 @@
+﻿// 11 y 12. Oración Palíndroma
+
+Console.Write("Oración: ");
+string original = Console.ReadLine().ToLower().Replace(" ", "");
+string reverso = "";
+for (int i = original.Length - 1; i >= 0; i--) reverso += original[i];
+Console.WriteLine(original == reverso ? "Es palíndroma" : "No es palíndroma");
