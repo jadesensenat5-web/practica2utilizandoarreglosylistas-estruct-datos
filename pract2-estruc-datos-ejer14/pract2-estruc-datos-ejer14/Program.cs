94175bb1b4b2ef589f12d647cce6e861eb23ac1d
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-11 09:00:00 -0400

    Create Program.cs
    ejercicio14

---
+++ pract2-estruc-datos-ejer14/pract2-estruc-datos-ejer14/Program.cs
@@ -0,0 +1,10 @@
+﻿// 14. Valor Máximo y Mínimo
+
+int[] lista = { 8, 3, 11, 2, 9 };
+int max = lista[0], min = lista[0];
+foreach (int n in lista)
+{
+    if (n > max) max = n;
+    if (n < min) min = n;
+}
+Console.WriteLine($"Máximo: {max}, Mínimo: {min}");
