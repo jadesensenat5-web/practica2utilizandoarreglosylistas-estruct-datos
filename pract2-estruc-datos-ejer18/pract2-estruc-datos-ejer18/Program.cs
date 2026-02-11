d730e004ac03217c87ffa6d2094ad308deeb1744
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-11 09:50:11 -0400

    Create Program.cs
    ejercicio18

---
+++ pract2-estruc-datos-ejer18/pract2-estruc-datos-ejer18/Program.cs
@@ -0,0 +1,4 @@
+﻿// 18. Eliminar duplicados
+int[] lista = { 1, 2, 2, 3, 4, 4, 5 };
+int[] sinDuplicados = lista.Distinct().ToArray(); // Usa System.Linq
+Console.WriteLine("Sin duplicados: " + string.Join(", ", sinDuplicados));
