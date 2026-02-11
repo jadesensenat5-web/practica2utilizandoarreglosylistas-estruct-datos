0c719fc5940f11d6cabd48e76778774a0fed4af5
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-11 10:14:36 -0400

    Create Program.cs
    ejercicio20

---
+++ pract2-estruc-datos-ejer20/pract2-estruc-datos-ejer20/Program.cs
@@ -0,0 +1,14 @@
+﻿// 20. Buscar posición (Índice) de un elemento
+int[] lista = { 5, 8, 12, 20 };
+Console.Write("Número a buscar: ");
+int buscar = int.Parse(Console.ReadLine());
+int posicion = -1;
+for (int i = 0; i < lista.Length; i++)
+{
+    if (lista[i] == buscar)
+    {
+        posicion = i;
+        break;
+    }
+}
+Console.WriteLine(posicion != -1 ? "Posición: " + posicion : "No encontrado");
