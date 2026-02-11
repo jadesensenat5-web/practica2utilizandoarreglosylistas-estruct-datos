107fd402b98d539da5d5770b52c862101d2d1fec
jadesensenat5-web <jadesensenat5@gmail.com>
2026-02-11 06:58:01 -0400

    Create Program.cs
    ejercicio9-10

---
+++ pract2-estruc-datos-ejer9/pract2-estruc-datos-ejer9/Program.cs
@@ -0,0 +1,7 @@
+﻿// 9 y 10. Palabra Palíndroma
+
+Console.Write("Palabra: ");
+string palabra = Console.ReadLine().ToLower();
+string reverso = "";
+for (int i = palabra.Length - 1; i >= 0; i--) reverso += palabra[i];
+Console.WriteLine(palabra == reverso ? "Es palíndroma" : "No es palíndroma");
